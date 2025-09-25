c5e2811de52c01ff8d991c36fa53dc5df9857ebc
joel gilmore <98565842+siqpush@users.noreply.github.com>
2025-09-25 08:14:00 -0700

    extra line removed

--- src/lib.rs
+++ src/lib.rs
@@ -318,7 +318,6 @@ where
     /// Get all pictures, tuple as (ext: String, data: Vec<u8>)
     #[cfg(feature = "picture")]
     fn pictures(&self) -> Option<Vec<(String, Vec<u8>)>>;
-
 }
 
 /// A trait to share spreadsheets reader functions across different `FileType`s
